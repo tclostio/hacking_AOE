f56c03724474bca5ddc7eec31255abf10f86bec0
tclostio <twclostio@protonmail.com>
2017-01-18 22:19:00 +0000

    .dtos exploit commit

---
+++ programming/src/dtors_sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void cleanup(void) __attribute__ ((destructor));
+
+main()
+{
+    printf("Some actions happen in the main() function...\n");
+    printf("and then when main() exits, the destructor is called...\n");
+    
+    exit(0);
+}
+
+void cleanup(void)
+{
+    printf("We cleanup now.\n");
+}
